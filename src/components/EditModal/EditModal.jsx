ee89726380a2ffe6fbcc5115f02369b4e469be1e
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-11-27 16:27:24 +0100

    setup form and image states for recipe editing

--- src/components/EditModal/EditModal.jsx
+++ src/components/EditModal/EditModal.jsx
@@ -6,6 +6,28 @@ import "./EditModal.css"
 
 
 const EditModal = () => {
+    const [formData, setFormData] = useState({
+        nom: '',
+        pays: '',
+        categorie: '',
+        image: '',
+        description: '',
+        tempsPreparation: '',
+        portions: '',
+        difficulte: '',
+        ingredients: [''],
+        etapes: ['']
+    })
+
+    const [imagePreview, setImagePreview] = useState(null);
+    const [ imageFile, setImageFile ] = useState(null);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const countries = [
+    'Maroc', 'Italie', 'Mexique', 'Japon', 'France', 'Inde', 
+    'Espagne', 'Grèce', 'Thaïlande', 'Chine', 'Liban', 'Turquie'
+  ]
+
+  
   return (
     <div>EditModal</div>
   )
