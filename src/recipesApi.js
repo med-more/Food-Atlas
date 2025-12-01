2d5b943a80e7b3c0f02e492fd79c0a2c5e230faf
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-12-01 15:36:53 +0100

    add getRecipeById function to fetch a single recipe by ID

--- src/recipesApi.js
+++ src/recipesApi.js
@@ -24,16 +24,6 @@ export const updateRecipe = async (id, recipe) =>{
 };
 
 
-// Fonction pour MODIFIER une recette existante
-export const updateRecipe = async (id, recipe) => {
-  try {
-    const response = await axios.put(`${API_URL}/${id}`, recipe);
-    return response.data;
-  } catch (error) {
-    console.error('Erreur lors la modification de recette:', error);
-    throw error;
-  }
-};
 
 // Fonction pour AJOUTER une recette
 export const addRecipe = async (recipe) => {
@@ -42,6 +32,18 @@ export const addRecipe = async (recipe) => {
     return response.data;
   } catch (error) {
     console.error('Erreur lors de l\'ajout de recette:', error);
+    throw error;
+  }
+};
+
+
+//function for get recipe by id
+export const getRecipeById = async (id) => {
+  try {
+    const response = await axios.get(`${API_URL}/${id}`);
+        return response.data;
+  } catch (error) {
+    console.error('Erreur lors de la récupération de la recette:', error);
     throw error;
   }
 };
