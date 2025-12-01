6108a9078f3afa4f35b11017c835320cf83623dd
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-12-01 16:58:05 +0100

    prepare the routings

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import AddRecipe from "./pages/AddRecipe/AddRecipe"
-import { Routes, Route } from 'react-router-dom' 
+import { Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import './App.css'
+
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+
+import Home from './pages/Home/Home'
+import Recipes from './pages/Recipes/Recipes'
+import RecipeDetails from './pages/RecipeDetails/RecipeDetails'
+import Contact from './pages/Contact/Contact'
+import AddRecipe from './pages/AddRecipe/AddRecipe'
+import ManageRecipes from './pages/ManageRecipes/ManageRecipes'
 
 function App() {
-
   return (
     <div className="app">
+      <Navbar />
       
       <main className="main-content">
-        <Routes>  
-          {/* Route pour ajouter une recette */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recettes" element={<Recipes />} />
+          <Route path="/recette/:id" element={<RecipeDetails />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/admin/ajouter" element={<AddRecipe />} />
+          <Route path="/admin/gestion" element={<ManageRecipes />} />
         </Routes>
       </main>
+
+      <Footer />
+      <Toaster position="top-right" />
     </div>
   )
 }
